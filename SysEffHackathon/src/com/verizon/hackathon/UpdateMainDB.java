9168ddca8eeed990c6c2a01dd08c950779eba0f6
VELA <abfygfgfg@gmail.com>
2015-09-30 08:16:22 -0700

    After Test

--- SysEffHackathon/src/com/verizon/hackathon/UpdateMainDB.java
+++ SysEffHackathon/src/com/verizon/hackathon/UpdateMainDB.java
@@ -1,31 +1,87 @@
 package com.verizon.hackathon;
 
-import java.io.File;
-import java.lang.management.ManagementFactory;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.util.Date;
-
-import com.sun.management.OperatingSystemMXBean;
-import com.sun.jna.platform.win32.Advapi32Util;
 import static com.sun.jna.platform.win32.WinReg.HKEY_LOCAL_MACHINE;
 
+import java.io.File;
+import java.lang.management.ManagementFactory;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+
+import com.sun.jna.platform.win32.Advapi32Util;
+import com.sun.management.OperatingSystemMXBean;
 
 public class UpdateMainDB {
 	
-	double cpuUsage;
-	double ramUsage;
-	double ramEff;
-	double hdPercent;
-	java.sql.Date date;
+	static int r=0;
+	static int s=0;
 
-	public void insertData(double cpuUsage,double ramUsage, double hdPercent,int user_id,Date date){
-		
-		
+	public static void insertData(double cpuUsage, double ramUsage, double hdPercent, int user_id) {
+
+		Connection con;
+		con = ConnectionUtil.getConnection();
+		PreparedStatement ps;
+
+		try {
+			ps = con.prepareStatement("INSERT INTO coll_usage VALUES (?,?,?,?,?)");
+			ps.setDouble(1, cpuUsage);
+
+			ps.setDouble(2, (100.00 - ramUsage));
+
+			ps.setDouble(3, hdPercent);
+
+			ps.setInt(4, 1001);
+
+			java.util.Date utilDate = new Date();
+
+			java.sql.Date date = new java.sql.Date(utilDate.getTime());
+
+			ps.setDate(5, date);
+
+			r=ps.executeUpdate();
+
+			ps.close();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
+
+	public static void insertSysInfo(int userId, String processorType, double ramCapacity, double hdSpace) {
+
+		Connection con;
+		con = ConnectionUtil.getConnection();
+		PreparedStatement ps;
+
+		String CPUprocessor = Advapi32Util.registryGetStringValue(HKEY_LOCAL_MACHINE,
+				"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0\\", "ProcessorNameString");
+
+		try {
+			ps = con.prepareStatement("INSERT INTO user_details VALUES (?,?,?,?)");
+			ps.setInt(1, 1001);
+
+			ps.setString(2, CPUprocessor);
+
+			ps.setDouble(3, ramCapacity);
+
+			ps.setDouble(4, hdSpace);
+
+			s=ps.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean) ManagementFactory
 				.getOperatingSystemMXBean();
 
@@ -43,8 +99,7 @@ public class UpdateMainDB {
 				double cpuUsage = bean.getSystemCpuLoad() * 100;
 				System.out.println("New Value: " + cpuUsage);
 
-				double ramUsage = (bean.getTotalPhysicalMemorySize() - bean.getFreePhysicalMemorySize())
-						/ 1000000000;
+				double ramUsage = (bean.getTotalPhysicalMemorySize() - bean.getFreePhysicalMemorySize()) / 1000000000;
 				System.out.println(ramUsage);
 
 				double ramEff = (ramUsage / (bean.getTotalPhysicalMemorySize() / 1000000000)) * 100;
@@ -60,61 +115,33 @@ public class UpdateMainDB {
 					}
 				}
 
-				System.out.println("Free Space " + freeSpace);
-				System.out.println("Total Space " + totalSpace);
+				// System.out.println("Free Space " + freeSpace);
+				// System.out.println("Total Space " + totalSpace);
 
-				java.util.Date utilDate = new Date();
-
-				java.sql.Date date = new java.sql.Date(utilDate.getTime());
-
-				java.sql.Connection con;
-				Class.forName("oracle.jdbc.driver.OracleDriver");
-				con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "password");
-				System.out.println("Connection established");
 				double hdPercent = ((totalSpace - freeSpace) / totalSpace) * 100;
 
 				System.out.println(bean.getSystemCpuLoad() * 100);
 				System.out.println(ramUsage);
 				System.out.println(hdPercent);
-				
-				 				
-				PreparedStatement ps = con.prepareStatement("INSERT INTO coll_usage VALUES (?,?,?,?,?)");
-				ps.setDouble(1, cpuUsage);
 
-				ps.setDouble(2, (100.00-ramUsage));
+				int userId = 1001;
 
-				ps.setDouble(3, hdPercent);
+				insertData(cpuUsage, ramUsage, hdPercent, userId);
 
-				ps.setInt(4, 1001);
-
-				ps.setDate(5, date);
-
-				ps.executeUpdate();
-				
-				ps.close();
-				
-				String CPUprocessor=Advapi32Util.registryGetStringValue(HKEY_LOCAL_MACHINE,
+				String CPUprocessor = Advapi32Util.registryGetStringValue(HKEY_LOCAL_MACHINE,
 						"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0\\", "ProcessorNameString");
-				
-				ps=con.prepareStatement("INSERT INTO user_details VALUES (?,?,?,?)");
-				
-				ps.setInt(1, 1001);
-				
-				ps.setString(2, CPUprocessor);
-				
-				ps.setDouble(3, bean.getTotalPhysicalMemorySize()/1000000000);
-				
-				ps.setDouble(4, totalSpace);
-				
-				ps.executeUpdate();
-				
+
+				double ramCapacity = bean.getTotalPhysicalMemorySize() / 1000000000;
+
+				insertSysInfo(userId, CPUprocessor, ramCapacity, totalSpace);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 
 		}
 
-
 	}
 
 }
+
+
--- SysEffHackathon/src/com/verizon/hackathon/UpdateMainDBtest.java
+++ SysEffHackathon/src/com/verizon/hackathon/UpdateMainDBtest.java
@@ -1,26 +1,52 @@
 package com.verizon.hackathon;
 
-import static org.junit.Assert.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import org.junit.After;
-import org.junit.BeforeClass;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+
+
+
 
 public class UpdateMainDBtest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		
-		
+	UpdateMainDB b;
+	
+	@Before
+	public void setUp() throws Exception {
+	b = new UpdateMainDB();
+	
 	}
 
 	@After
 	public void tearDown() throws Exception {
+ 
+		//pst.deleteData(100003 );
+		Connection con = ConnectionUtil.getConnection();
+		System.out.println("Connection established TEAR");
+		PreparedStatement ps = con.prepareStatement("delete from coll_usage where user_id = 1003");
+		PreparedStatement pd = con.prepareStatement("delete from user_details where user_id=1003");
+		ps.executeUpdate();
+		pd.executeUpdate();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void testInsertData() {
+		
+		
+		b.insertData(10.33, 94.00, 22.40, 1003);	
+		Assert.assertEquals(1, b.r);
+		}
 
+	@Test
+	public void testInsertSysInfo(){
+		
+		b.insertSysInfo(1003, "i5 OctaCore", 4, 120);
+		
+		Assert.assertEquals(1, b.s);
+	}
 }
